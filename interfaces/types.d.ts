abd9fae176209f22b0ca4794c9a50f22a06981e6
Nebur23 <yvesnyemb7@gmail.com>
2025-12-19 08:12:13 +0100

    added VaultExtensionMethodsOperations prop in vault interface

--- interfaces/types.d.ts
+++ interfaces/types.d.ts
@@ -28,6 +28,41 @@ interface IVault {
   Name: string;
   GUID: string;
   URL?: string;
+
+
+  VaultExtensionMethodsOperations: {
+    /**
+     * Is the specified vault extension method available.
+     * @param params - The parameters object
+     * @param params.method_identifier - The method identifier
+     * @returns A promise that resolves to an object containing:
+     *   - exists: Information about the extension method existence
+     *   - is_active: Information about the state of the extension method (active/not active)
+     */
+    IsExtensionMethodAvailable(params: { method_identifier: string }): Promise<{ exists: boolean; is_active: boolean }>
+
+    /**
+     * Runs the specified vault extension method. Prefer this method over 'ExecuteExtensionMethod' and 'ExecuteExtensionMethodEx' methods.
+     * @param params - The parameters object
+     * @param params.application_id - The guid of the target application. Set all-zero guid if the extension method is not bound to an application but VBScript event handler
+     * @param params.extension - The identifier of the vault extension implementing the extension method. Set empty string if the extension method is not bound to an application but VBScript event handler
+     * @param params.instance - The identifier of the vault extension instance that implements the extension method. Set empty string if the extension method is not bound to an application but VBScript event handler
+     * @param params.method_name - The method identifier. With application this is the extension method name. With VBScript event handlers this is the event handler name of 'VaultExtensionMethod' handler that receives the call
+     * @param params.input - The method input as a clear text. Use empty string if compressed input field is used instead
+     * @param params.compressed_input - The method input as compressed data (utf8 text as zlib-compressed byte array). Use empty data if the clear-text input is set
+     * @returns A promise that resolves to an object containing:
+     *   - output: The method output as a clear text. Empty string if compressed output field is used instead
+     *   - compressed_output: The method input as compressed data (utf8 text as zlib-compressed byte array). Empty data if the clear-text output was used
+     */
+    RunExtensionMethod(params: {
+      application_id: string;
+      extension: string;
+      instance: string;
+      method_name: string;
+      input: string;
+      compressed_input: Uint8Array
+    }): Promise<{ output: string; compressed_output: Uint8Array }>
+  }
 }
 
 interface DialogUIParams {
